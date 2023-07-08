314b713b3ae3bf9c2a8981532f70de294dca102c
Jessica Miller <jmil690@wgu.edu>
2023-07-08 17:52:38 -0500

    add display_name to custom user model

--- clonestagram/users/models.py
+++ clonestagram/users/models.py
@@ -5,6 +5,7 @@ from django.utils.translation import gettext_lazy as _
 
 class CustomUser(AbstractUser):
     email = models.EmailField(_("email address"), unique=True)
+    display_name = models.CharField(max_length=30, null=False)
 
     def __str__(self):
         return self.email
