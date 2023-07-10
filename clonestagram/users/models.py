7c6cf3a20aee1b6ecc21dd40baaf4635c4f9df15
Jessica Miller <jmil690@wgu.edu>
2023-07-09 19:39:19 -0500

    squash user migrations; add new custom fields

--- clonestagram/users/models.py
+++ clonestagram/users/models.py
@@ -6,6 +6,12 @@ from django.utils.translation import gettext_lazy as _
 class CustomUser(AbstractUser):
     email = models.EmailField(_("email address"), unique=True)
     display_name = models.CharField(max_length=30, null=False)
+    profile_picture = models.ImageField(null=True, upload_to="static/photos")
+    following = models.ManyToManyField(
+        "self", blank=True, related_name="followers", symmetrical=False
+    )
+    bio = models.TextField(max_length=150, blank=True, default="Bio")
+    website = models.URLField(max_length=100, blank=True)
 
     def __str__(self):
         return self.email
